8ba96c3cc0b139898fd1628c7ff8a29dfaab17bc
JuanDPAffi <juan.pinilla@affi.net>
2025-11-16 16:57:40 -0500

    prueba interceptor

--- src/app/auth/interceptors/auth-interceptor.ts
+++ src/app/auth/interceptors/auth-interceptor.ts
@@ -1,19 +1,34 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
+  const router = inject(Router);
+
   const token = auth.getToken();
 
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next(cloned);
-  }
+  // 1) Adjuntar Authorization si hay token
+  const authReq = token
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    : req;
 
-  return next(req);
+  // 2) Manejar errores globales
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        // Usuario ya no válido: usuario inactivo, sesión inválida, token vencido, etc.
+        auth.logout();              // limpia local y/o avisa al backend
+        router.navigate(['/auth/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
