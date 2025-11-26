727891c04ee2ae8e139a32f81e337fbd457ea9b3
JuanDPAffi <juan.pinilla@affi.net>
2025-11-26 14:57:28 -0500

    fix: ambiente prod

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.prod';
 
 // ... (Tus interfaces LoginPayload, RegisterPayload, etc. se mantienen igual)
 export interface LoginPayload {
